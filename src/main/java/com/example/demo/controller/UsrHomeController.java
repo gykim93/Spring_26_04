a0fabd4c84ae4394ee9c5804a5ed3af624d7f644
gykim93 <gykim1993@gmail>
2026-04-02 21:07:19 +0900

    lombok 오류 해결

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,57 +1,27 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 @Controller
 public class UsrHomeController {
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1", "내용1");
+		
+		return article;
+	}
+}
+@AllArgsConstructor
+@Data
+class Article{
 
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수");
-		list.add("영수");
-		
-		return list;
-	}
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String,Object> map = new HashMap<>();
-		map.put("철수의 나이 ", 12);
-		map.put("영수의 나이: ", 13);
-		
-		return map;
-	}	
-	
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 3.14;
-	}	
-	
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}	
-	
-	@RequestMapping("/usr/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "aaaaaa";
-	}
-	
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 99;
-	}
+	int id;
+	String title;
+	String body;
 }
