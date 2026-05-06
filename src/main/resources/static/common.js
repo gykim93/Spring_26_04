f4fbb1b32021e95fe2d3319e8a1e71cda54d0ffd
gykim93 <gykim1993@gmail.com>
2026-05-06 20:47:28 +0900

    검색 박스 구현, 누락되는 데이터 없도록, 검색 기준 추가

---
+++ src/main/resources/static/common.js
@@ -0,0 +1,8 @@
+$('select[data-value').each(function() {
+    const $el = $(el);
+    defaultValue = $el.attr('data-value').trim();
+
+    if (defaultValue.length > 0) {
+        $el.val(defaultValue);
+    }
+});
